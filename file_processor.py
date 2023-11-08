403b7cfbde58993184cdd8a39d315f123526b630
ahall <ahall@inventprise.com>
2023-11-08 09:29:33 -0800

    Add final data format

--- file_processor.py
+++ file_processor.py
@@ -1,17 +1,39 @@
 import pandas as pd 
 import os
 import glob
+import xlrd
+from pandas_pract import create_output_workbook, get_concat_dataframe
+
+'''
 
 # use glob to get all excel files
-path = os.getcwd()
+dir_name = 'excel_files'
+path = f'{os.getcwd()}\\{dir_name}' 
+
 excel_files = glob.glob(os.path.join(path, '*.xls'))
+
 
 # loop over list of excel files
 for excel_file in excel_files:
 
-	# read and
-	# process the excel file here
+	input_workbook = xlrd.open_workbook(excel_file)
 
-	# print filename
-	print('File Name:', excel_file.split("\\")[-1])
+	# get title from cell 'B2'
+	title = input_workbook.sheet_by_index(0).cell_value(rowx=1, colx=1) \
+				.split(':')[0]
+	
+	create_output_workbook(title, input_workbook)
+
+'''
+dir_name = 'excel_files'
+path = f'{os.getcwd()}\\{dir_name}'
+file_path = os.path.join(path, 'a.xls')
+
+# TEST SINGLE FILE
+# input_workbook = xlrd.open_workbook(file_path)
+# df = get_concat_dataframe(input_workbook)
+# print(df.shape)
+# print(df.head())
+	
+create_output_workbook(file_path)
 
--- pandas_pract.py
+++ pandas_pract.py
@@ -1,92 +1,181 @@
 import pandas as pd 
-# import matplotlib.pyplot as plt
-# from xlsxwriter import Workbook
+import xlrd
 
-excel_file = 'test.xls'
-
-# B2
-# F4_Klebsiella_K102_02agf_23aug2023: DataLog SP, PV, Output, 											
-
-# Read Excel and select a single cell (and make it a header for a column)
-title = pd.read_excel(excel_file, 'Sheet1', index_col=None, usecols = "B", header = 10, nrows=0)
-
-serotype = title.columns.values[0].split(':, 1')[0]
 
 COLS_TO_PARSE = [0, 2, 6]
-COL_TITLES = ['Time Stamp', 'Loop Name', 'Process Value']
+COL_TITLES_BEFORE_PIVOT = ['Time Stamp', 'Loop Name', 'Process Value']
+STARTROW = 3
+NUM_PARAMS = 6
+
+COL_TITLES_AFTER_PIVOT = ['Time Stamp', 'pH', 'DO', 'Agitation', 'CO2', 'O2', 'Temperature']
+# SEQUENCE OF UNITS, order important
+UNITS = ['Hour', 'Unit', '%', 'RPM', 'SLPM', 'SLPM', '°C']
+
+# col_rename_dict = {
+# 	'1-pH_Dev1': 'pH',
+# 	'2-DO_Dev1': 'DO',
+# 	'Agitation_Dev1': 'Agitation',
+# 	'S-Air_Dev1': 'Aeration',
+# 	'S-CO2_Dev1': 'CO2',
+# 	'S-O2_Dev1': 'Oxygen',
+# 	'Temp_Dev1': 'Temperature',
+# 	'1-pH_Dev2': 'pH',
+# 	'2-DO_Dev2': 'DO',
+# 	'Agitation_Dev2': 'Agitation',
+# 	'S-Air_Dev2': 'Aeration',
+# 	'S-CO2_Dev2': 'CO2',
+# 	'S-O2_Dev2': 'Oxygen',
+# 	'Temp_Dev2': 'Temperature',
+# }
 
 # create DataFrame object from excel file
 
-df_sheet1 = pd.read_excel(
-	excel_file, 
-	sheet_name=0, 
-	skiprows=5,
-	usecols=COLS_TO_PARSE,
-	header=None,
-	names=COL_TITLES,
+def get_concat_dataframe(xlrd_book):
+
+	num_sheets = xlrd_book.nsheets 
+
+	dataframes = []
+	for sheet in range(num_sheets):
+		skiprows = 5 if sheet == 0 else 0
+		df = create_dataframe(xlrd_book, sheet, skiprows) 
+		dataframes.append(df)
+
+	bioreactor_data = pd.concat(dataframes) \
+					    .dropna() \
+					    .pivot(index='Time Stamp', columns='Loop Name', values='Process Value')
+
+	# drop any unwanted columns and rename remaining columns
+	cols_to_drop = [col for col in list(bioreactor_data.columns) if 'Pump' in col]
+	bioreactor_data = bioreactor_data.drop(columns=cols_to_drop)  
+									# .rename(columns=col_rename_dict)
+
+	return bioreactor_data
+
+
+
+def create_dataframe(xlrd_book, sheet_name, skiprows):
+	return pd.read_excel(
+		xlrd_book,
+		sheet_name=sheet_name,
+		skiprows=skiprows,
+		usecols=COLS_TO_PARSE,
+		header=None,
+		names=COL_TITLES_BEFORE_PIVOT,
 	)
-df_sheet2 = pd.read_excel(
-	excel_file, 
-	sheet_name=1, 
-	usecols=COLS_TO_PARSE,
-	header=None,
-	names=COL_TITLES,
-	)
-bioreactor_data = pd.concat([df_sheet1, df_sheet2]) \
-					.dropna() \
-					.pivot(index='Time Stamp', columns='Loop Name', values='Process Value')
 
 
-cols_to_drop = [col for col in list(bioreactor_data.columns) if 'Pump' in col]
-bioreactor_data = bioreactor_data.drop(columns=cols_to_drop)
 
-col_rename_dict = {
-	'1-pH_Dev1': 'pH',
-	'2-DO_Dev1': 'DO',
-	'Agitation_Dev1': 'Agitation',
-	'S-Air_Dev1': 'Aeration',
-	'S-CO2_Dev1': 'CO2',
-	'S-O2_Dev1': 'Oxygen',
-	'Temp_Dev1': 'Temperature',
-}
+def create_output_workbook(file_path):
 
-bioreactor_data = bioreactor_data.rename(columns=col_rename_dict)
+	input_workbook = xlrd.open_workbook(file_path)
+
+	title = input_workbook.sheet_by_index(0).cell_value(rowx=1, colx=1) \
+				.split(':')[0]
+
+	# NEED TO CLOSE XLRD WORKBOOK?????
+
+	data = get_concat_dataframe(input_workbook)
+
+	# write data to excel sheet
+	writer = pd.ExcelWriter(f'{title}.xlsx', engine='xlsxwriter')
+	data.to_excel(writer, 
+		          sheet_name='Sheet1', 
+		          startrow=STARTROW, 
+		          freeze_panes=(STARTROW + 1,NUM_PARAMS),
+		          )
+	
+	# create workbook 
+	wb = writer.book
+
+	# add title to cell A1 in Sheet1
+	ws = wb.get_worksheet_by_name('Sheet1')
+	bold = wb.add_format({'bold': True})
+	
+	ws.write('A1', title, bold)
+
+	for col, name in enumerate(COL_TITLES_AFTER_PIVOT):
+		ws.write(STARTROW - 1, col, name, bold)
+
+	for col, unit in enumerate(UNITS):
+		ws.write(STARTROW, col, unit, bold)
+
+	# copy columns headers to row 2 0-indexed
+	# write units to row 3 0-indexed
+	# units = ['hour', 'Unit', '%', 'RPM', '%', 'SLPM', 'SLPM', '°C']
+	# for unit in units:
+	# 	ws.write()
+
+	# create chartsheet
+	cs = wb.add_chartsheet() 
+
+	# create chart object
+	chart = wb.add_chart({ 'type': 'scatter', })
+
+	# configure chart
+	chart = configure_chart(chart, data.shape, title)
+
+	# add chart to chartsheet
+	cs.set_chart(chart)
+
+	writer.close()
 
 
-# write data to excel sheet
+def add_series_to_chart(chart, data_shape):
 
-writer = pd.ExcelWriter('test_graphs2.xlsx', engine='xlsxwriter')
-bioreactor_data.to_excel(writer, sheet_name='Sheet1')
-wb = writer.book
-ws = writer.sheets['Sheet1']
+	# configure the series from the dataframe data
+	(max_row, max_col) = data_shape
 
-# create a chart object
-chart = wb.add_chart({ 'type': 'scatter', })
+	for col in range(1, max_col):
 
-# configure chart axes and title
-chart.set_x_axis({ 'name': 'EFT, min', })
-chart.set_y_axis({ 'name': 'Agitation (RPM), DO (%)', })
-chart.set_title({ 'name': f'{serotype} Fermentor Conditions', })
+		has_y2_axis = col == 3
+		series_config = get_series_config(col, max_row, has_y2_axis, STARTROW)
 
-# configure the series from the dataframe data
-(max_row, max_col) = bioreactor_data.shape
+		chart.add_series(series_config)
 
-for col in range(1, max_col):
-	chart.add_series({
-		'name': ['Sheet1', 0, col],
- 		'categories': ['Sheet1', 1, 0, max_row, 0],
- 		'values': ['Sheet1', col, max_col, max_row, col],
- 		'marker': { 
- 			'type': 'automatic',
- 			'size': 1, 
- 		 },
- 		 'line': { 
- 		 	'dash_type': 'solid',
- 		 	'size': 1,
- 		 },
-	})
+	return chart
 
-ws.insert_chart('C1', chart)
 
-writer.close()
+def get_series_config(col, max_row, has_y2_axis, startrow):
+
+	'''
+	name = column title
+	categories = time stamp values
+	values = column values in {name}
+	[{sheet_title}, row_min, col_min, row_max, col_max]
+	'''
+	config = {
+			'name': ['Sheet1', startrow, col], # row, col
+	 		'categories': ['Sheet1', startrow + 2, 0, startrow + max_row - 1, 0], # max_row - 1 because 0-indexed
+	 		'values': ['Sheet1', startrow + 2, col, startrow + max_row - 1, col], 
+	 		'marker': { 
+	 			'type': 'automatic',
+	 			'size': 1, 
+	 		 },
+	 		 'line': { 
+	 		 	'dash_type': 'solid',
+	 		 	'size': 1,
+	 		 },
+	}
+
+	return config | { 'y2_axis': 1 } if has_y2_axis else config
+
+
+def configure_chart(chart, data_shape, chart_title):
+
+	# configure chart axes and title
+	chart.set_x_axis({ 'name': 'EFT, hour', })
+	chart.set_y_axis({ 'name': 'DO, %', })
+	chart.set_y2_axis({ 'name': 'Agitation, RPM'})
+
+	chart.set_title({ 'name': f'{chart_title} Fermentor Conditions', })
+	chart.set_legend({ 'position': 'bottom' })
+
+	# configure the series from the dataframe data
+	chart = add_series_to_chart(chart, data_shape)
+
+
+	return chart
+
+
+
 
